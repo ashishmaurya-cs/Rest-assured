efd51b0f49d503b5f2ebd2ef7d13bb0548003bbd
Ram Ashish Maurya <ramashishmaurya1997@gmail.com>
2025-06-16 23:49:51 +0530

    updated HemCrustLib.java

--- src/test/java/com/ashish/restassured/PutMethod.java
+++ src/test/java/com/ashish/restassured/PutMethod.java
@@ -8,7 +8,7 @@ import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.*;
 
-public class PutPATCHTest {
+public class PutMethod {
 	@Test
 	public void testPut() {
 		JSONObject request = new JSONObject();
@@ -29,34 +29,6 @@ public class PutPATCHTest {
 			.statusCode(200)
 			.log().all();
 		
-	}
-	
-	@Test
-	public void testPATCH() {
-		JSONObject request = new JSONObject();
-		request.put("name", "Ajeet");
-//		request.put("job", "Automation Tester");
-		
-		System.out.println(request.toJSONString());
-		baseURI = "https://reqres.in/api" ;
-		
-		given()
-			.header("Content-Type", "application/json")
-			.contentType(ContentType.JSON)
-			.accept(ContentType.JSON)
-			.body(request.toJSONString())
-		.when()
-			.patch("/users/2")
-		.then()
-			.statusCode(200)
-			.log().all();
-		
-	}
-	
-	@Test
-	public void testDelete() {
-		baseURI = "https://reqres.in/api";
-		when().delete("/users/2").then().statusCode(204);
 	}
 	
 }
