471a8d1093942ec8f0c0fbbfc5351c0784dd102c
Ram Ashish Maurya <ramashishmaurya1997@gmail.com>
2026-01-06 19:26:39 +0530

    Updated LaptopPojo.java

--- src/test/java/dummy/restapiexample/PostTest.java
+++ src/test/java/dummy/restapiexample/PostTest.java
@@ -10,9 +10,22 @@ public class PostTest {
     @Test
     public  void postTest(){
 
+        //prepare body
+        LaptopPojo laptopPojo = new LaptopPojo();
+        laptopPojo.setName("HP");
+
+        // static inner class preparation
+        LaptopPojo.Data data= new LaptopPojo.Data();
+        data.setYear("2024");
+        data.setPrice("30000");
+        data.setCpuModel("Intel core i3");
+        data.setHardDiskSize("500 GB");
+
+        laptopPojo.setData(data);
+
         Response rs = RestAssured.given()
                 .contentType(ContentType.JSON)
-                .body()
+                .body(laptopPojo)
                 .when().baseUri("https://api.restful-api.dev/objects")
                 .post()
                 .then()
