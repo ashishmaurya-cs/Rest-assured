f8efdb9fad4e2ac0ec1d58b005310d30806a9e18
Ram Ashish Maurya <ramashishmaurya1997@gmail.com>
2026-01-09 20:11:35 +0530

    updated Data.java

--- src/test/java/dummy/restapiexample/Data.java
+++ src/test/java/dummy/restapiexample/Data.java
@@ -1,4 +1,40 @@
 package dummy.restapiexample;
 
 public class Data {
+    private  String year;
+    private String price;
+    private String cpuModel;
+    private String hardDiskSize;
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getCpuModel() {
+        return cpuModel;
+    }
+
+    public void setCpuModel(String cpuModel) {
+        this.cpuModel = cpuModel;
+    }
+
+    public String getHardDiskSize() {
+        return hardDiskSize;
+    }
+
+    public void setHardDiskSize(String hardDiskSize) {
+        this.hardDiskSize = hardDiskSize;
+    }
 }
