bd5bcbf3604fe43a5aa90542a8cc5335c213ee7a
Ram Ashish Maurya <ramashishmaurya1997@gmail.com>
2026-01-10 17:53:56 +0530

    updated PutExample.java

--- src/test/java/dummy/restapiexample/PutExample.java
+++ src/test/java/dummy/restapiexample/PutExample.java
@@ -3,6 +3,7 @@ package dummy.restapiexample;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PutExample {
@@ -22,19 +23,23 @@ public class PutExample {
 
         laptopPojo.setData(data);
 
-
-
-        RestAssured.baseURI = "https://api.restful-api.dev/objects/ff8081819782e69e019ba330df5c0158";
+        RestAssured.baseURI = "https://api.restful-api.dev";
         Response rs = RestAssured.given()
                 .contentType(ContentType.JSON)
-                .when()
                 .body(laptopPojo)
-                .put()
+                .when()
+                .put("/objects/ff8081819782e69e019ba330df5c0158")
                 .then()
+                .log().all()
                 .extract()
                 .response();
 
         System.out.println(rs.jsonPath().getString("name"));
+
+     // Assertions
+     Assert.assertEquals(rs.jsonPath().getString("name"), "HP");
+     Assert.assertEquals(rs.jsonPath().getInt("data.year"), 2019);
+     Assert.assertEquals(rs.jsonPath().getString("data.cpuModel"), "Intel Core i9");
 
     }
 
