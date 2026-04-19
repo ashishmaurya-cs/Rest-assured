7cc3ea0e0cfb57cda6fb6df60581704197a90ca5
Ram Ashish Maurya <ramashishmaurya1997@gmail.com>
2026-04-19 20:59:55 +0530

    added GetExample.java

---
+++ src/test/java/practice/GetExample.java
@@ -0,0 +1,21 @@
+package practice;
+
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+import org.testng.annotations.Test;
+
+public class GetExample {
+    @Test
+    public void getEmployee() {
+        Response rs = given()
+                .header("Content-Type", "application/json")
+                .when()
+                .get("https://dummy.restapiexample.com/employees")
+                .then()
+                .statusCode(200).extract().response();
+
+        System.out.println(rs.asString());
+    }
+}
